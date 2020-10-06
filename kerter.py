deae6c27373ba2e8915cd4b00f14ac54c3a3f5ee
julcsi95 <juliasuto@gmail.com>
2020-10-06 19:10:47 +0200

    Háromszög területe kész.

--- kerter.py
+++ kerter.py
@@ -3,3 +3,8 @@ def haromszogKerulete():
 	b=float(input("Kérem a háromszög 'b' oldalát [cm]:"))
 	c=float(input("Kérem a háromszög 'c' oldalát [cm]:"))
 	return float (a+b+c)
+	
+def haromszogTerulete():
+	a=float(input("Kérem a háromszög 'a' oldalát [cm]:"))
+	ma=float(input("Kérem a háromszög 'm' magasságát [cm]:"))
+	return float ((ma*a)/2)
