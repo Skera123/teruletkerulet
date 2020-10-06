fbdb0e4db30c12897152ad7c9538dfaa22875244
Tinka05 <vivien920321@gmail.com>
2020-10-06 19:13:58 +0200

    nyolcszog cucc

--- kerter.py
+++ kerter.py
@@ -1,3 +1,11 @@
 Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> 
+def nyolcszogKerulet ():
+a=float(input("Kérem a nyolcszög oldalát [cm]: "))
+return float (8*a)
+
+def nyolcszogTerulet ():
+a=float(input("Kérem a nyolcszög oldalát [cm]: ")
+r=float(input("Kérem a nyolcszög sugarát [cm]: ")
+return float (4*a*r) 
