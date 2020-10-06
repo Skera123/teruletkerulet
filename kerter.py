cc346f648cc3214b00bba57266e2a11b3ee3514f
julcsi95 <juliasuto@gmail.com>
2020-10-06 19:06:00 +0200

    háromszög kerülete kész

--- kerter.py
+++ kerter.py
@@ -1,3 +1,5 @@
-Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+def haromszogKerulete():
+	a=float(input("Kérem a háromszög 'a' oldalát [cm]:"))
+	b=float(input("Kérem a háromszög 'b' oldalát [cm]:"))
+	c=float(input("Kérem a háromszög 'c' oldalát [cm]:"))
+	return float (a+b+c)
